025ec71c529f478dd50f8f3195fe8c579190df9e
Jay Young <jyoungta@cisco.com>
2017-05-22 12:46:21 -0700

    added better comments in the example file

--- docs/example.py
+++ docs/example.py
@@ -1,10 +1,18 @@
 import BootIntegrityValidator
 
+# Open the Known good values files as and pass in 'bytes' to initialization function
 kgv = open("example_kgv.json", "rb")
 bi = BootIntegrityValidator.BootIntegrityValidator(known_good_values=kgv.read())
 
+# Open the files that have show platform sudi and show platform integrity output and pass in as strings
+# to the validate function
 show_plat_suid = open("example_show_plat_sudi.txt", "r")
 show_plat_int = open("example_show_plat_int.txt", "r")
+
+# The validate function will raise specific exceptions if validation fails
+# or it is unable to validate the output.
+# If validation is successful the function will execute to completion without error
+# and returns None
 
 try:
     bi.validate(show_platform_integrity_cmd_output=show_plat_int.read(),
@@ -18,3 +26,4 @@ except BootIntegrityValidator.BootIntegrityValidator.ProductNotFound:
     print("Product in cli output not mapped to 'product' in known_good_values")
 except BootIntegrityValidator.BootIntegrityValidator.ValidationException:
     print("Validation failed")
+
