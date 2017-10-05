4ba390f23f9d94f2622d3f360cddbbaa3d6d2a11
Jay Young <jyoungta@cisco.com>
2017-10-05 15:15:11 -0400

    extra platforms

--- BootIntegrityValidator/package_meta_data.py
+++ BootIntegrityValidator/package_meta_data.py
@@ -1,5 +1,5 @@
 __title__ = "BootIntegrityValidator"
-__version__ = "0.2.7.7"
+__version__ = "0.2.7.8"
 __author__ = "Jay Young"
 __author_email__ = "jyoungta@cisco.com"
 __copyright__ = "Copyright 2017, Cisco Systems, Inc"
--- BootIntegrityValidator/platforms.py
+++ BootIntegrityValidator/platforms.py
@@ -1,5 +1,6 @@
 import six
 
+
 class ProductFamily(object):
     _ISR4K = "ISR4000"
     _ASR1K = "ASR1000"
@@ -17,15 +18,26 @@ class ProductFamily(object):
                  'ISR4331/K9': _ISR4K,
                  'ISR4321/K9': _ISR4K,
                  'ISR4221/K9': _ISR4K,
+                 'WS-C3650-8X24PD': _CAT3K,
                  'WS-C3650-8X24UQ': _CAT3K,
                  'WS-C3650-12X48UQ': _CAT3K,
+                 'WS-C3650-12X48UR': _CAT3K,
+                 'WS-C3650-12X48UZ': _CAT3K,
                  'WS-C3650-24PD': _CAT3K,
                  'WS-C3650-24PDM': _CAT3K,
+                 'WS-C3650-24PS': _CAT3K,
                  'WS-C3650-24TS': _CAT3K,
                  'WS-C3650-48FQM': _CAT3K,
+                 'WS-C3650-48PD': _CAT3K,
+                 'WS-C3650-48PQ': _CAT3K,
                  'WS-C3650-48PS': _CAT3K,
+                 'WS-C3650-48TD': _CAT3K,
+                 'WS-C3650-48TQ': _CAT3K,
+                 'WS-C3650-48TS': _CAT3K,
+                 'WS-C3850-12S': _CAT3K,
                  'WS-C3850-12X48U': _CAT3K,
                  'WS-C3850-12XS': _CAT3K,
+                 'WS-C3850-24S': _CAT3K,
                  'WS-C3850-24XS': _CAT3K,
                  'WS-C3850-24XU': _CAT3K,
                  'WS-C3850-48XS': _CAT3K}
